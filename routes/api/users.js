fe6711c91f65e907a6ad2d1aaa15ea3d94bd1a91
Jian Guo <guojian0706@gmail.com>
2020-09-01 16:05:47 -0500

    Email $ password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,30 @@ router.post('/register', (req, res) => {
         })
 });
 
+// @route   GET api/users/login
+// @desc    Lotin User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password; // Password that the user typed in (plain text)
+
+    // Find user by email
+    User.findOne({email}) // Mongoose method to find one record
+        .then(user => {
+            // Check for user
+            if (!user) {
+                return res.status(404).json({email: 'User not found.'});
+            }
+            // Check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({msg: 'Success'});
+                    } else {
+                        return res.status(400).json({password: 'Password incorrect.'});
+                    }
+                });
+        });
+});
+
 module.exports = router;
